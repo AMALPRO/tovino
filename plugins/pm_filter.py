b0e791d1ffedb010307c9577a85a02cd097959fe
joker153 <77005994+joker153@users.noreply.github.com>
2023-04-14 20:17:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -846,9 +846,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k =  await msg.reply_photo(
-             photo=SPELL_IMG, 
-             caption="I couldn't find anything related to that. Check your spelling"
+        k = await msg.reply("I couldn't find anything related to that. Check your spelling")
         await asyncio.sleep(8)
         await k.delete()
         return
