970d1355515339681628a58d6569206ef5296856
joker153 <77005994+joker153@users.noreply.github.com>
2023-04-14 21:29:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -816,7 +816,7 @@ async def advantage_spell_chok(msg):
     g_s += await search_gagala(msg.text)
     gs_parsed = []
     if not g_s:
-     k = await photo.reply(
+     k = await msg.reply_photo(
          photo=SPELL_IMG,
          caption="I couldn't find any movie in that name."
      )       
@@ -849,7 +849,7 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist))  # removing duplicates
     if not movielist:
-        k = await photo.reply(
+        k = await msg.reply_photo(
             photo=SPELL_IMG,
             caption="I couldn't find anything related to that. Check your spelling"
         )
